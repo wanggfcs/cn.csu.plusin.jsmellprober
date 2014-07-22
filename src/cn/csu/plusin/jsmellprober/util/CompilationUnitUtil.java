66299763ac72f15b4ae21365f714fc3fe7561aaf
jd_xianwc501 <xianwc@sina.com>
2014-07-22 19:55:43 +0800

    add TCC

--- src/cn/csu/plusin/jsmellprober/util/CompilationUnitUtil.java
+++ src/cn/csu/plusin/jsmellprober/util/CompilationUnitUtil.java
@@ -17,30 +17,32 @@ public class CompilationUnitUtil {
 		long start = System.currentTimeMillis();
 		try {
 
-			MRCVisitor MRCv = new MRCVisitor();
-			MPCVisitor MPCv = new MPCVisitor();
-			DACVisitor DACV = new DACVisitor();
-			DAC2Visitor DAC2V = new DAC2Visitor();
-			OCMECVisitor OCMECV = new OCMECVisitor();
-			WMCVisitor WMCV = new WMCVisitor();
-			NOLVVisitor NOLV = new NOLVVisitor();
-			
-			WMCV.setUnit(cu);
-			WMCV.setSource(icu.getSource());
-			cu.accept(MRCv);
-			cu.accept(MPCv);
-			cu.accept(DACV);
-			cu.accept(DAC2V);
-			cu.accept(OCMECV);
-			cu.accept(WMCV);
-			cu.accept(NOLV);
-			System.out.println("MRC: "+MRCv.getResult());
-			System.out.println("MPC: "+MPCv.getResult());
-			System.out.println("DAC: "+DACV.getResult());
-			System.out.println("DAC2: "+DAC2V.getResult());
-			System.out.println("OCMEC: "+OCMECV.getResult());
-			System.out.println("WMC: "+WMCV.getResult());
-			System.out.println("NOLV: "+NOLV.getResult());
+//			MRCVisitor MRCv = new MRCVisitor();
+//			MPCVisitor MPCv = new MPCVisitor();
+//			DACVisitor DACV = new DACVisitor();
+//			DAC2Visitor DAC2V = new DAC2Visitor();
+//			OCMECVisitor OCMECV = new OCMECVisitor();
+//			WMCVisitor WMCV = new WMCVisitor();
+//			NOLVVisitor NOLV = new NOLVVisitor();
+			TCCVisitor TCCV = new TCCVisitor();
+//			WMCV.setUnit(cu);
+//			WMCV.setSource(icu.getSource());
+//			cu.accept(MRCv);
+//			cu.accept(MPCv);
+//			cu.accept(DACV);
+//			cu.accept(DAC2V);
+//			cu.accept(OCMECV);
+//			cu.accept(WMCV);
+//			cu.accept(NOLV);
+			cu.accept(TCCV);
+//			System.out.println("MRC: "+MRCv.getResult());
+//			System.out.println("MPC: "+MPCv.getResult());
+//			System.out.println("DAC: "+DACV.getResult());
+//			System.out.println("DAC2: "+DAC2V.getResult());
+//			System.out.println("OCMEC: "+OCMECV.getResult());
+//			System.out.println("WMC: "+WMCV.getResult());
+//			System.out.println("NOLV: "+NOLV.getResult());
+			System.out.println("TCCV: "+TCCV.getResult());
 			//
 
 		} catch (Exception e) {
--- src/cn/csu/plusin/jsmellprober/visitor/TCCVisitor.java
+++ src/cn/csu/plusin/jsmellprober/visitor/TCCVisitor.java
@@ -25,6 +25,7 @@ public class TCCVisitor extends ASTVisitor {
 	private Set<MethodParam> methodList = new HashSet<MethodParam>();
 
 	public boolean visit(TypeDeclaration node) {
+		System.out.println(node.getName().getIdentifier());
 		FieldDeclaration[] fd = node.getFields();
 		for (FieldDeclaration fdTemp : fd) {
 			int md = fdTemp.getModifiers();
@@ -38,10 +39,11 @@ public class TCCVisitor extends ASTVisitor {
 			}
 		}
 
-		return false;
+		return true;
 
 	}
-	//获得方法遍历
+
+	// 获得方法遍历
 	public boolean visit(MethodDeclaration node) {
 		param = new MethodParam();
 		node.accept(new MethodVisitor());
@@ -57,7 +59,7 @@ public class TCCVisitor extends ASTVisitor {
 
 	class MethodVisitor extends ASTVisitor {
 		public boolean visit(SimpleName node) {
-			//获得全部变量
+			// 获得全部变量
 			if (node.resolveBinding() instanceof IVariableBinding) {
 				IVariableBinding itb = (IVariableBinding) node.resolveBinding();
 				param.addMethodVarible(itb.getKey());
@@ -67,43 +69,49 @@ public class TCCVisitor extends ASTVisitor {
 
 		}
 	}
-	//计算阶乘数
-	private int fac(int n){
-		int i,t=1;
-		if(n>0){
-			for(i=1;i<=n;i++){
-				t=t*i;
+
+	// 计算阶乘数
+	private int fac(int n) {
+		int i, t = 1;
+		if (n > 0) {
+			for (i = 1; i <= n; i++) {
+				t = t * i;
 			}
 		}
 		return t;
 	}
-	
-	//计算组合数
-	public  int zuhe(int n ,int r){
-		return fac(n)/(fac(n-r)*fac(r));
+
+	// 计算组合数
+	public int zuhe(int n, int r) {
+		return fac(n) / (fac(n - r) * fac(r));
 	}
-	
-	
+
 	public int getResult() {
-		int pairsOfMethod =0,methodNum = methodList.size();
-		int pairsOfComAttrMethod=0;
-		pairsOfMethod = zuhe(methodNum,2);
-		//遍历类的所有属性
-		for(String akey: attributeSet){
-			//属性在多少方法中出现
-			int attributeUseTime=0;
-			//变量所有方法，看是否有使用属性
-			for(MethodParam mp:methodList){
-				if(mp.getMethodVaribleList().contains(akey)){
-					attributeUseTime++;
+		int pairsOfMethod = 0;
+		int methodNum = methodList.size();
+		if (methodNum > 2) {
+
+			int pairsOfComAttrMethod = 0;
+			pairsOfMethod = zuhe(methodNum, 2);
+			// 遍历类的所有属性
+			for (String akey : attributeSet) {
+				// 属性在多少方法中出现
+				int attributeUseTime = 0;
+				// 变量所有方法，看是否有使用属性
+				for (MethodParam mp : methodList) {
+					if (mp.getMethodVaribleList().contains(akey)) {
+						attributeUseTime++;
+					}
 				}
+				// 计算出现的组合
+				if (attributeUseTime >= 2) {
+					pairsOfComAttrMethod += zuhe(attributeUseTime, 2);
+				}
+
 			}
-			//计算出现的组合
-			pairsOfComAttrMethod+=zuhe(attributeUseTime,2);
-			
+			return (int)((pairsOfComAttrMethod / (double)pairsOfMethod) * 100);
 		}
-		
-		return pairsOfComAttrMethod/pairsOfMethod*100;
+		return 0;
 
 	}
 }
